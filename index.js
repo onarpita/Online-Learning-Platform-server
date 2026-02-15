91f02c5c56f0a231ad6f6006bb93a7db249d5d28
onarpita <onarpita14@gmail.com>
2026-02-15 20:34:40 +0600

    create a mongoclient

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8gmleuq.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Online learning platform server is running");
+});
+
