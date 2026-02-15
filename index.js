1b3f2f67883f93fa8e3e85ded1c3ee2a87a2342b
onarpita <onarpita14@gmail.com>
2026-02-15 20:32:35 +0600

    midlleware added

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8gmleuq.mongodb.net/?appName=Cluster0`;
