ede24f3c15fcb2444659e481a14a33fab2b0d87d
onarpita <onarpita14@gmail.com>
2026-02-15 20:40:24 +0600

    section of delete api

--- index.js
+++ index.js
@@ -85,4 +85,13 @@ async function run() {
       res.send(result);
     });
 
+        // Delete API
+    app.delete("/courses/:id", async (req, res) => {
+      const id = req.params;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await myCourses.deleteOne(query);
+      res.send(result);
+    });
+
 
